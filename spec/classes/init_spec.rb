7c44de8e85f7414c14446073d53e4395f32403c3
Frank Wall <fw@moov.de>
2020-05-07 13:52:57 +0200

    tests: assorted style fixes to pet rubocop

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,84 +1,105 @@
 require 'spec_helper'
 
 describe 'sonarqube' do
-
   let(:sonar_properties) { '/usr/local/sonar/conf/sonar.properties' }
 
-  context "when installing version 4", :compile do
-    let(:params) {{ :version => '4.5.5' }}
-    it { should contain_wget__fetch('download-sonar').with_source('https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-4.5.5.zip') }
+  context 'when installing version 4', :compile do
+    let(:params) { { version: '4.5.5' } }
+
+    it { is_expected.to contain_wget__fetch('download-sonar').with_source('https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-4.5.5.zip') }
   end
 
-  context "when crowd configuration is supplied", :compile do
-    let(:params) { { :crowd => {
-      'application' => 'crowdapplication',
-      'service_url' => 'crowdserviceurl',
-      'password'    => 'crowdpassword',
-    } } }
+  context 'when crowd configuration is supplied', :compile do
+    let :params do
+      {
+        crowd:
+          {
+            'application' => 'crowdapplication',
+            'service_url' => 'crowdserviceurl',
+            'password'    => 'crowdpassword',
+          },
+      }
+    end
 
-    it 'should generate sonar.properties config for crowd' do
-      should contain_file(sonar_properties).with_content(%r[sonar\.authenticator\.class: org\.sonar\.plugins\.crowd\.CrowdAuthenticator])
-      should contain_file(sonar_properties).with_content(%r[crowd\.url: crowdserviceurl])
-      should contain_file(sonar_properties).with_content(%r[crowd\.application: crowdapplication])
-      should contain_file(sonar_properties).with_content(%r[crowd\.password: crowdpassword])
+    it 'generates sonar.properties config for crowd' do
+      is_expected.to contain_file(sonar_properties).with_content(%r{sonar\.authenticator\.class: org\.sonar\.plugins\.crowd\.CrowdAuthenticator})
+      is_expected.to contain_file(sonar_properties).with_content(%r{crowd\.url: crowdserviceurl})
+      is_expected.to contain_file(sonar_properties).with_content(%r{crowd\.application: crowdapplication})
+      is_expected.to contain_file(sonar_properties).with_content(%r{crowd\.password: crowdpassword})
     end
   end
 
-  context "when no crowd configuration is supplied", :compile do
-    it { should contain_file(sonar_properties).without_content("crowd") }
+  context 'when no crowd configuration is supplied', :compile do
+    it { is_expected.to contain_file(sonar_properties).without_content('crowd') }
   end
 
-  context "when unzip package is not defined", :compile do
-    it { should contain_package('unzip').with_ensure('present') }
+  context 'when unzip package is not defined', :compile do
+    it { is_expected.to contain_package('unzip').with_ensure('present') }
   end
 
-  context "when unzip package is already defined", :compile do
-    let(:pre_condition) { %Q[
-      package { 'unzip': ensure => installed }
-    ] }
+  context 'when unzip package is already defined', :compile do
+    let :pre_condition do
+      "package { 'unzip': ensure => installed }"
+    end
 
-    it { should contain_package('unzip').with_ensure('installed') }
+    it { is_expected.to contain_package('unzip').with_ensure('installed') }
   end
 
-  context "when ldap local users configuration is supplied", :compile do
-    let(:params) { { :ldap => {
-      'url'          => 'ldap://myserver.mycompany.com',
-      'user_base_dn' => 'ou=Users,dc=mycompany,dc=com',
-      'local_users'  => 'foo',
-    } } }
+  context 'when ldap local users configuration is supplied', :compile do
+    let :params do
+      {
+        ldap:
+          {
+            'url'          => 'ldap://myserver.mycompany.com',
+            'user_base_dn' => 'ou=Users,dc=mycompany,dc=com',
+            'local_users'  => 'foo',
+          },
+      }
+    end
 
-    it { should contain_file(sonar_properties).with_content(/sonar.security.localUsers=foo/) }
-    it { should contain_file(sonar_properties).with_content(/sonar.security.realm=LDAP/) }
-    it { should contain_file(sonar_properties).with_content(/ldap.url=ldap:\/\/myserver.mycompany.com/) }
-    it { should contain_file(sonar_properties).with_content(/ldap.user.baseDn: ou=Users,dc=mycompany,dc=com/) }
+    it { is_expected.to contain_file(sonar_properties).with_content(%r{sonar.security.localUsers=foo}) }
+    it { is_expected.to contain_file(sonar_properties).with_content(%r{sonar.security.realm=LDAP}) }
+    it { is_expected.to contain_file(sonar_properties).with_content(%r{ldap.url=ldap:\/\/myserver.mycompany.com}) }
+    it { is_expected.to contain_file(sonar_properties).with_content(%r{ldap.user.baseDn: ou=Users,dc=mycompany,dc=com}) }
   end
 
-  context "when ldap local users configuration is supplied as array", :compile do
-    let(:params) { { :ldap => {
-      'url'          => 'ldap://myserver.mycompany.com',
-      'user_base_dn' => 'ou=Users,dc=mycompany,dc=com',
-      'local_users' => ['foo','bar'],
-    } } }
+  context 'when ldap local users configuration is supplied as array', :compile do
+    let :params do
+      {
+        ldap:
+          {
+            'url'          => 'ldap://myserver.mycompany.com',
+            'user_base_dn' => 'ou=Users,dc=mycompany,dc=com',
+            'local_users'  => ['foo', 'bar'],
+          },
+      }
+    end
 
-    it { should contain_file(sonar_properties).with_content(/sonar.security.localUsers=foo,bar/) }
-    it { should contain_file(sonar_properties).with_content(/sonar.security.realm=LDAP/) }
-    it { should contain_file(sonar_properties).with_content(/ldap.url=ldap:\/\/myserver.mycompany.com/) }
-    it { should contain_file(sonar_properties).with_content(/ldap.user.baseDn: ou=Users,dc=mycompany,dc=com/) }
+    it { is_expected.to contain_file(sonar_properties).with_content(%r{sonar.security.localUsers=foo,bar}) }
+    it { is_expected.to contain_file(sonar_properties).with_content(%r{sonar.security.realm=LDAP}) }
+    it { is_expected.to contain_file(sonar_properties).with_content(%r{ldap.url=ldap:\/\/myserver.mycompany.com}) }
+    it { is_expected.to contain_file(sonar_properties).with_content(%r{ldap.user.baseDn: ou=Users,dc=mycompany,dc=com}) }
   end
 
-  context "when no ldap local users configuration is supplied", :compile do
-    let(:params) { { :ldap => {
-      'url'          => 'ldap://myserver.mycompany.com',
-      'user_base_dn' => 'ou=Users,dc=mycompany,dc=com',
-    } } }
-    it { should contain_file(sonar_properties).without_content(/sonar.security.localUsers/) }
-    it { should contain_file(sonar_properties).with_content(/sonar.security.realm=LDAP/) }
-    it { should contain_file(sonar_properties).with_content(/ldap.url=ldap:\/\/myserver.mycompany.com/) }
-    it { should contain_file(sonar_properties).with_content(/ldap.user.baseDn: ou=Users,dc=mycompany,dc=com/) }
+  context 'when no ldap local users configuration is supplied', :compile do
+    let :params do
+      {
+        ldap:
+          {
+            'url'          => 'ldap://myserver.mycompany.com',
+            'user_base_dn' => 'ou=Users,dc=mycompany,dc=com',
+          },
+      }
+    end
+
+    it { is_expected.to contain_file(sonar_properties).without_content(%r{sonar.security.localUsers}) }
+    it { is_expected.to contain_file(sonar_properties).with_content(%r{sonar.security.realm=LDAP}) }
+    it { is_expected.to contain_file(sonar_properties).with_content(%r{ldap.url=ldap:\/\/myserver.mycompany.com}) }
+    it { is_expected.to contain_file(sonar_properties).with_content(%r{ldap.user.baseDn: ou=Users,dc=mycompany,dc=com}) }
   end
 
-  context "when no ldap configuration is supplied", :compile do
-    it { should contain_file(sonar_properties).without_content(/sonar.security/) }
-    it { should contain_file(sonar_properties).without_content(/ldap./) }
+  context 'when no ldap configuration is supplied', :compile do
+    it { is_expected.to contain_file(sonar_properties).without_content(%r{sonar.security}) }
+    it { is_expected.to contain_file(sonar_properties).without_content(%r{ldap.}) }
   end
 end
--- spec/classes/runner_spec.rb
+++ spec/classes/runner_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe 'sonarqube::runner' do
   context 'when installing' do
-    it { should create_class('sonarqube::runner::install') }
-    it { should create_class('sonarqube::runner::config') }
+    it { is_expected.to create_class('sonarqube::runner::install') }
+    it { is_expected.to create_class('sonarqube::runner::config') }
   end
 end
