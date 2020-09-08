4cfb0418f07c065e354dacae77f8ba302219116f
root <root@puppetmaster.xxs.vagrant>
2020-09-08 09:47:12 +0000

    fixing package install

--- manifests/install/package.pp
+++ manifests/install/package.pp
@@ -4,28 +4,18 @@ class sonarqube::install::package {
     home => $sonarqube::home,
   }
 
-  # this class intended to be used from sonarqube::install class
-  assert_private()
+  ## this class intended to be used from sonarqube::install class
+  #assert_private()
 
-  $arch         = $::sonarqube::arch
-  $installdir   = $::sonarqube::installdir
+  #$arch         = $::sonarqube::arch
+  #$installdir   = $::sonarqube::installdir
   $package_name = $::sonarqube::package_name
   $version      = $::sonarqube::version
 
-  $arch_dir = "${installdir}/bin/${arch}"
-  $sonar_script = "${arch_dir}/sonar.sh"
-  $pid_file = "${arch_dir}/SonarQube.pid"
+  #$arch_dir = "${installdir}/bin/${arch}"
+  #$sonar_script = "${arch_dir}/sonar.sh"
+  #$pid_file = "${arch_dir}/SonarQube.pid"
 
-  package { $package_name:
-    ensure => $version,
-  }
-  -> exec {'echo variables':
-    command => "echo ${sonarqube::installroot}",
-  }
-  -> exec { 'move sonar to correct location':
-    command => "cp /opt/sonarqube-* ${sonarqube::installroot} && chown -R \
-      ${sonarqube::user}:${sonarqube::group} ${sonarqube::installroot}/${sonarqube::distribution_name}-${sonarqube::version} && chown -R ${sonarqube::user}:${sonarqube::group} ${sonarqube::home}",
-  }
 
   # Create user and group
   user { $sonarqube::user:
@@ -38,7 +28,9 @@ class sonarqube::install::package {
     ensure => present,
     system => $sonarqube::user_system,
   }
-
+  -> package { $package_name:
+    ensure => $version,
+  }
   # Create folder structure
   -> file { $sonarqube::home:
     ensure => directory,
@@ -50,11 +42,21 @@ class sonarqube::install::package {
   -> file { $sonarqube::installdir:
     ensure => link,
     target => "${sonarqube::installroot}/${sonarqube::distribution_name}-${sonarqube::version}",
-    notify => Class['sonarqube::service'],
+    #notify => Class['sonarqube::service'],
   }
   -> sonarqube::move_to_home { 'data': }
   -> sonarqube::move_to_home { 'extras': }
   -> sonarqube::move_to_home { 'extensions': }
   -> sonarqube::move_to_home { 'logs': }
+
+  -> exec { 'move sonar to correct location':
+    command => "cp -r /opt/sonarqube-* ${sonarqube::installroot} && chown -R \
+      ${sonarqube::user}:${sonarqube::group} ${sonarqube::installroot} && chown -R ${sonarqube::user}:${sonarqube::group} ${sonarqube::home} || true",
+    notify => Class['sonarqube::service'],
+  }
+
+  file { $sonarqube::plugin_dir:
+    ensure => directory,
+  }  
 
 }
